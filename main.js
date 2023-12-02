04c05005a2963ad5d2ee3e41523ca31c2b32947c
JeanPSL <jeanpunk01@gmail.com>
2023-12-02 02:15:30 -0300

    Se agrega el proyecto

---
+++ main.js
@@ -0,0 +1,50 @@
+// Juego del piedra papel o tijeras 
+
+let volverATirar = true;
+
+while (volverATirar) {
+    console.log("Ingresar opcion PIEDRA, PAPEL O TIJERA")
+
+    let opcionUsuario1
+    do {
+        opcionUsuario1 = ingresarOpcion("Ingrese que opciones desea tirar");
+    } while (!opcionEsCorrecta(opcionUsuario1));
+
+    let opcionUsuario2
+    do {
+        opcionUsuario2 = ingresarOpcion("Ingrese que opciones desea tirar");
+    } while (!opcionEsCorrecta(opcionUsuario2));
+
+    var resultado = validarGanador(opcionUsuario1, opcionUsuario2)
+    
+    console.log(resultado)
+    
+    volverATirar = confirm("Desea volver a jugar?")
+}
+
+function opcionEsCorrecta (opcionElegida) {
+
+    if (opcionElegida !== "piedra" && opcionElegida !== "papel" && opcionElegida !== "tijera") {
+        return false
+    }
+    return true
+}
+
+function ingresarOpcion (mensaje) { 
+    return prompt(mensaje).toLowerCase()
+}
+
+function validarGanador (opcionUsuario1, opcionUsuario2) {
+
+    if (opcionUsuario1 === opcionUsuario2) {
+        return "Empate";
+    } else if (
+        (opcionUsuario1 == "piedra" && opcionUsuario2 == "tijera") ||
+        (opcionUsuario1 == "papel" && opcionUsuario2 == "piedra") ||
+        (opcionUsuario1 == "tijera" && opcionUsuario2 == "papel")
+    ) {
+        return "Gana el usuario 1";
+    } else {
+        return "Gana el usuario 2";
+    }
+}
