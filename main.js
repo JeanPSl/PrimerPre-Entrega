7359c984402b1267437ebfb8714839b0671d4897
JeanPSL <jeanpunk01@gmail.com>
2023-12-05 16:04:23 -0300

    feat: Se agrega funcionalidad en la que se puede jugar con la maquina

--- main.js
+++ main.js
@@ -1,50 +1,106 @@
 // Juego del piedra papel o tijeras 
 
 let volverATirar = true;
+let valueOpcionMaquina;
+let resultado;
 
 while (volverATirar) {
-    alert("Ingresar opcion PIEDRA, PAPEL O TIJERA")
+    let opcionUsuario1;
+    let opcionUsuario2;
 
-    let opcionUsuario1
-    do {
-        opcionUsuario1 = ingresarOpcion("Ingrese que opciones desea tirar");
-    } while (!opcionEsCorrecta(opcionUsuario1));
+    if (confirm("Desea jugar contra un amigo?")) {
+        do {
+            opcionUsuario1 = ingresarOpcion("Ingrese que opcion desea tirar \n1.- PIEDRA\n2.- PAPEL\n3.- TIJERA");
+        } while (!opcionEsCorrecta(opcionUsuario1));
 
-    let opcionUsuario2
-    do {
-        opcionUsuario2 = ingresarOpcion("Ingrese que opciones desea tirar");
-    } while (!opcionEsCorrecta(opcionUsuario2));
+        do {
+            opcionUsuario2 = ingresarOpcion("Ingrese que opcion desea tirar \n1.- PIEDRA\n2.- PAPEL\n3.- TIJERA");
+        } while (!opcionEsCorrecta(opcionUsuario2));
 
-    let resultado = validarGanador(opcionUsuario1, opcionUsuario2)
-    
-    alert(resultado)
-
-    volverATirar = confirm("Desea volver a jugar?")
-}
-
-function opcionEsCorrecta (opcionElegida) {
-
-    if (opcionElegida !== "piedra" && opcionElegida !== "papel" && opcionElegida !== "tijera") {
-        return false
-    }
-    return true
-}
-
-function ingresarOpcion (mensaje) { 
-    return prompt(mensaje).toLowerCase().trim()
-}
-
-function validarGanador (opcionUsuario1, opcionUsuario2) {
-
-    if (opcionUsuario1 === opcionUsuario2) {
-        return "Empate";
-    } else if (
-        (opcionUsuario1 == "piedra" && opcionUsuario2 == "tijera") ||
-        (opcionUsuario1 == "papel" && opcionUsuario2 == "piedra") ||
-        (opcionUsuario1 == "tijera" && opcionUsuario2 == "papel")
-    ) {
-        return "Gana el usuario 1";
+        validarOpcionSeleccionada(opcionUsuario1, opcionUsuario2);
     } else {
-        return "Gana el usuario 2";
+        alert("Jugaras contra la maquina");
+
+        do {
+            opcionUsuario1 = ingresarOpcion("Ingrese que opcion desea tirar \n1.- PIEDRA\n2.- PAPEL\n3.- TIJERA");
+        } while (!opcionEsCorrecta(opcionUsuario1));
+
+        validarOpcionSeleccionada(opcionUsuario1, null);
+        alert("La maquina tiro: " + valueOpcionMaquina);
+    }
+
+    alert(resultado);
+
+    volverATirar = confirm("Desea volver a jugar?");
+}
+
+function opcionEsCorrecta(opcionElegida) {
+    return opcionElegida === 1 || opcionElegida === 2 || opcionElegida === 3;
+}
+
+function ingresarOpcion(mensaje) {
+    return Number(prompt(mensaje).toLowerCase().trim());
+}
+
+function validarOpcionSeleccionada(opcionUsuario1, opcionUsuario2) {
+    let valueOpcionUser1;
+    let valueOpcionUser2;
+
+    switch (opcionUsuario1) {
+        case 1:
+            valueOpcionUser1 = "piedra";
+            break;
+        case 2:
+            valueOpcionUser1 = "papel";
+            break;
+        case 3:
+            valueOpcionUser1 = "tijera";
+            break;
+    }
+
+    if (opcionUsuario2 !== null) {
+        switch (opcionUsuario2) {
+            case 1:
+                valueOpcionUser2 = "piedra";
+                break;
+            case 2:
+                valueOpcionUser2 = "papel";
+                break;
+            case 3:
+                valueOpcionUser2 = "tijera";
+                break;
+        }
+    } else {
+        let valorRandom = parseInt(Math.random() * 3 + 1);
+
+        switch (valorRandom) {
+            case 1:
+                valueOpcionMaquina = "piedra";
+                break;
+            case 2:
+                valueOpcionMaquina = "papel";
+                break;
+            case 3:
+                valueOpcionMaquina = "tijera";
+                break;
+        }
+
+        return validarGanador(valueOpcionUser1, valueOpcionMaquina);
+    }
+
+    return validarGanador(valueOpcionUser1, valueOpcionUser2);
+}
+
+function validarGanador(opcionUser1, opcionUser2) {
+    if (opcionUser1 === opcionUser2) {
+        resultado = "Empate";
+    } else if (
+        (opcionUser1 === "piedra" && opcionUser2 === "tijera") ||
+        (opcionUser1 === "papel" && opcionUser2 === "piedra") ||
+        (opcionUser1 === "tijera" && opcionUser2 === "papel")
+    ) {
+        resultado = "Gana el usuario 1";
+    } else {
+        resultado = "Gana el usuario 2";
     }
 }
